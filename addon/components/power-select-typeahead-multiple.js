bc64b8688c78b80042ec9d7badc0ea3011727cee
Chuck Shipman <chshipma@cisco.com>
2019-02-06 16:20:02 -0600

    Added a `power-select-typeahead-multiple` variant

---
+++ addon/components/power-select-typeahead-multiple.js
@@ -0,0 +1,47 @@
+import Component from '@ember/component';
+import { computed } from '@ember/object';
+import layout from '../templates/components/power-select-typeahead-multiple';
+
+export default Component.extend({
+  tagName: '',
+  layout,
+  tabindex: -1,
+  triggerComponent: 'power-select-typeahead/trigger',
+  beforeOptionsComponent: null,
+  searchEnabled: false,
+  loadingMessage: null,
+  noMatchesMessage: null,
+
+  // CPs
+  concatenatedTriggerClasses: computed('triggerClass', function() {
+    let classes = ['ember-power-select-typeahead-trigger'];
+    let passedClass = this.get('triggerClass');
+    if (passedClass) {
+      classes.push(passedClass);
+    }
+    return classes.join(' ');
+  }),
+
+  concatenatedDropdownClasses: computed('dropdownClass', function() {
+    let classes = ['ember-power-select-typeahead-dropdown'];
+    let passedClass = this.get('dropdownClass');
+    if (passedClass) {
+      classes.push(passedClass);
+    }
+    return classes.join(' ');
+  }),
+
+  actions: {
+    onKeyDown(select, e) {
+      let action = this.get('onkeydown');
+
+      // if user passes `onkeydown` action
+      if (!action || action(select, e) !== false) {
+        // if escape, then clear out selection
+        if (e.keyCode === 27) {
+          select.actions.choose(null);
+        }
+      }
+    }
+  }
+});
